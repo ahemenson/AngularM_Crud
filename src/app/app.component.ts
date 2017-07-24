8fda6f918517a7635b713dca11d1cfe98e3c2228
ahemenson <ahemenson.cavalcante@dce.ufpb.br>
2017-07-24 17:26:26 -0300

    final da aula 3 portas

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { AngularmService, EntityTypesComponent } from 'angularm';
+
+import { describeDomain } from './entities';
+import { defineRules } from './rules';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent extends EntityTypesComponent {
+  title = 'app';
+
+  constructor(angularm: AngularmService) {
+    super();
+    describeDomain(angularm);
+    this.entityTypes = angularm.listEntityTypes();
+
+    defineRules(angularm);
+
+    angularm.create('book', { name: 'Refactoring', author: 'Fowler' });
+    angularm.create('book', { name: 'Patterns', author: 'GoF' });
+    angularm.create('product', { id: '1', description: 'Banana' });
+    
+
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { AngularmModule } from 'angularm';
+
+import { AppComponent } from './app.component';
+import { ListEntityTypeComponent } from './list-entity-type/list-entity-type.component';
+import { NewEntityComponent } from './new-entity/new-entity.component';
+import { EditEntityComponent } from './edit-entity/edit-entity.component';
+import { ShowEntityComponent } from './show-entity/show-entity.component';
+import { EditFormLineComponent } from './edit-entity/edit-form-line';
+
+export const routes: Routes = [
+  { path: ':entitytypename', component: ListEntityTypeComponent },
+  { path: ':entitytypename/new', component: NewEntityComponent },
+  { path: ':entitytypename/:key', component: ShowEntityComponent },
+  { path: ':entitytypename/:key/edit', component: EditEntityComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEntityTypeComponent,
+    NewEntityComponent,
+    EditEntityComponent,
+    ShowEntityComponent,
+    EditFormLineComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
+    AngularmModule
+  ],
+  providers: [],
+  entryComponents: [
+    EditFormLineComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
